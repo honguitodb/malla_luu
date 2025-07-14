bf094c35c67bff2a0072900e21d34d9b20f87ba5
honguitodb <lucivar518@gmail.com>
2025-07-14 07:52:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const malla = [
     ramos: [
       { nombre: "Cálculo 3", requisitos: ["Cálculo 2", "Álgebra lineal"]},
       { nombre: "Termodinámica", requisitos: ["Cálculo 2", "Química para ingeniería"]},
-      { nombre: "Laboratorio termodinámica", requisitos: ["Cálculo 2", "Química para inegniería"] },
+      { nombre: "Laboratorio termodinámica", requisitos: ["Cálculo 2", "Química para ingeniería"] },
       { nombre: "Biológico" },
       { nombre: "Ecuaciones diferenciales", requisitos: ["Cálculo 2", "Álgebra lineal"] },
       { nombre: "Ecología integral/sustentabilidad" }
